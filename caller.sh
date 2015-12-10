1e99820b877c42f18493f7a4470e426926cb9d66
bazyk <ced.bazyk@gmail.com>
2015-12-10 14:54:36 -0500

    first commit

---
+++ caller.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+echo "############# Starting Script #############"
+echo 'Hello from Bash'
+
+java helloWorld
+python3 helloWorld.py
+ruby helloWorld.rb
+julia helloworld.jl
+echo "############ Script end ###################"
+
